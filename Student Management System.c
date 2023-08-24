b61bd46340b28f1533e72df92daa093c5a8a7449
sandeshkhadka10 <sandeshkhadka551@gmail.com>
2023-08-24 23:56:22 +0545

    first commit

---
+++ Student Management System.c
@@ -0,0 +1,397 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Student 
+{
+    char name[50];
+    int id;
+    char year[50];
+    char address[50];
+    char email[50];
+    char phonenumber[50];
+    float gpa;
+};
+
+const char *filename = "students_records.dat";
+
+void AddStudent();
+void DisplayAllStudents();
+void SearchStudent();
+void EditStudentDetails();
+void DeleteStudent();
+
+int main() 
+{
+    char choice, x;
+    while (1) 
+    {
+        system("cls");
+        printf("\t\t\t\t\t<== Student Management System ==>\n");
+        printf("\t\t*********************************************************************************\n");
+        flag:
+        fflush(stdin);
+        printf("\n\t\ta. Add Student\n");
+        printf("\t\tb. Display All Students\n");
+        printf("\t\tc. Search Student\n");
+        printf("\t\td. Edit Student Details\n");
+        printf("\t\te. Delete Student\n");
+        printf("\t\tf. Exit\n");
+        printf("\n\t\tEnter your choice: ");
+        scanf(" %c", &choice); 
+        
+        switch (choice) 
+        {
+            case 'a':
+                AddStudent();
+                break;
+                
+            case 'b':
+                DisplayAllStudents();
+                break;
+                
+            case 'c':
+                SearchStudent();
+                break;
+                
+            case 'd':
+                EditStudentDetails();
+                break;
+                
+            case 'e':
+                DeleteStudent();
+                break;
+                
+            case 'f':
+                printf("\t\tPress Y to exit and N to cancel: ");
+                fflush(stdin);
+                scanf(" %c", &x); // Added a space before %c to consume newline characters
+                if (x == 'Y' || x == 'y')
+                {
+                    exit(0);
+                }
+                else if (x == 'N' || x == 'n')
+                {
+                    printf("\t\tPlease read the option clearly\n");
+                    goto flag;
+                }
+                else
+                {
+                    printf("\t\t Please press Y or N only\n");
+                    goto flag;
+                }
+                break;
+
+            default:
+                printf("\n\t\tInvalid Choice....\n");
+                printf("\t\tPlease Enter from A to F\n");
+                goto flag;
+        }
+        printf("\n\t\t\t\t\tPress any key to continue");
+        getch(); 
+    }
+    return 0;
+}
+
+void AddStudent()
+{
+    char another;
+    do
+    {
+        struct Student newstudent;
+        system("cls");
+        printf("\t\t\t\t\t<== Add Student Information ==>\n\n");
+       
+        printf("\n\t\tName: ");
+        fflush(stdin);
+        gets(newstudent.name);
+
+        printf("\n\t\tId no: ");
+        scanf("%d", &newstudent.id);
+
+        printf("\n\t\tCourse and Semester: ");
+        fflush(stdin);
+        gets(newstudent.year);
+
+        printf("\n\t\tAddress: ");
+        fflush(stdin);
+        gets(newstudent.address);
+        
+        printf("\n\t\tEmail: ");
+        fflush(stdin);
+        gets(newstudent.email);
+        
+        printf("\n\t\tPhone no: ");
+        fflush(stdin);
+        gets(newstudent.phonenumber);
+    
+        printf("\n\t\tGPA: ");
+        fflush(stdin);
+        scanf("%f", &newstudent.gpa);
+        printf("\t\t____________________________________________________");
+       
+        FILE *file = fopen(filename, "ab");
+        if (file == NULL) 
+        {
+            printf("\t\tUnable to open file.\n");
+            return;
+        }
+
+        fwrite(&newstudent, sizeof(struct Student), 1, file);
+        fclose(file);
+        
+        fflush(stdin);
+        printf("\n\t\tDo you want to add another record?(Y/N): ");
+        scanf(" %c", &another);
+    } while (another == 'y' || another == 'Y');
+}
+
+void DisplayAllStudents()
+{
+    system("cls");
+    printf("\t\t\t\t\t<== Student Records ==>\n\n");
+    
+    FILE *file = fopen(filename,"rb");
+    if (file == NULL) 
+    {
+        printf("\t\tUnable to open file.\n");
+        return;
+    }
+    
+    struct Student newstudent;
+    
+    while (fread(&newstudent, sizeof(struct Student), 1, file))
+    {
+        printf("\n\t\tStudent Name: %s", newstudent.name);
+        printf("\n\t\tId: %d", newstudent.id);
+        printf("\n\t\tClass and Semester: %s", newstudent.year);
+        printf("\n\t\tAddress: %s", newstudent.address);
+        printf("\n\t\tEmail:%s",newstudent.email);
+        printf("\n\t\tPhone number:%s",newstudent.phonenumber);
+        printf("\n\t\tGPA: %.2f", newstudent.gpa);
+        printf("\n\t\t____________________________________");
+    }   
+    fclose(file);
+}
+
+void SearchStudent()
+{
+    int search=0, found=0;
+    system("cls");
+    printf("\t\t\t\t\t<== Search Student ==>\n\n");
+    
+    FILE *file = fopen(filename,"rb");
+    if (file == NULL) 
+    {
+        printf("\t\tUnable to open file.\n");
+        return;
+    }
+    
+    struct Student newstudent;
+    
+    printf("\n\t\tEnter the id you want to search: ");
+    scanf("%d", &search);
+    
+    while (fread(&newstudent, sizeof(struct Student), 1, file))
+    {
+        if (newstudent.id == search)
+        {
+            found = 1;
+            printf("\n\t\tStudent Name: %s", newstudent.name);
+            printf("\n\t\tId: %d", newstudent.id);
+            printf("\n\t\tClass and Semester: %s", newstudent.year);
+            printf("\n\t\tAddress: %s", newstudent.address);
+            printf("\n\t\tEmail:%s",newstudent.email);
+            printf("\n\t\tPhone number:%s",newstudent.phonenumber);
+            printf("\n\t\tGPA: %.2f", newstudent.gpa);
+            printf("\n\t\t____________________________________");
+        }
+    }
+    
+    if (found != 1)
+    {
+        printf("\n\t\tRecord not found\n");
+    }
+    
+    fclose(file);
+}
+
+
+
+   void EditStudentDetails()
+{
+    system("cls");
+    printf("\t\t\t\t\t<== Edit Student Details ==>\n\n");
+    
+    int id = 0, found = 0;
+    printf("\t\tEnter ID to Edit: ");
+    scanf("%d", &id);
+
+    struct Student updatedStudent;
+    
+    FILE *file = fopen(filename, "rb+");
+    if (file == NULL) 
+    {
+        printf("\t\tUnable to open file.\n");
+        return;
+    }
+
+    struct Student newstudent;
+
+    while (fread(&newstudent, sizeof(struct Student), 1, file))
+    {
+        if (newstudent.id == id)
+        {
+            updatedStudent = newstudent; // Load existing data for editing
+            found = 1;
+            break;
+        }
+    }
+
+    if (!found) 
+    {
+        printf("\n\t\tStudent with ID %d not found.\n", id);
+        fclose(file);
+        return;
+    }
+
+    fclose(file);
+    
+    flag:
+    printf("\n\t\tSelect fields to update:\n");
+    printf("\t\t1. Name\n");
+    printf("\t\t2. ID\n");
+    printf("\t\t3. Semester\n");
+    printf("\t\t4. Address\n");
+    printf("\t\t5. Email\n");
+    printf("\t\t6. Phone Number\n");
+    printf("\t\t7. GPA\n");
+    printf("\t\tEnter your choice (1-7): ");
+
+    int choice;
+    scanf("%d", &choice);
+
+    switch (choice)
+    {
+        case 1:
+            printf("\n\t\tEnter Updated Name: ");
+            fflush(stdin);
+            gets(updatedStudent.name);
+            break;
+
+        case 2:
+            printf("\n\t\tEnter Updated ID: ");
+            scanf("%d", &updatedStudent.id);
+            break;
+
+        case 3:
+            printf("\n\t\tEnter Updated Semester: ");
+            fflush(stdin);
+            gets(updatedStudent.year);
+            break;
+
+        case 4:
+            printf("\n\t\tEnter Updated Address: ");
+            fflush(stdin);
+            gets(updatedStudent.address);
+            break;
+
+        case 5:
+            printf("\n\t\tEnter Updated Email: ");
+            fflush(stdin);
+            gets(updatedStudent.email);
+            break;
+
+        case 6:
+            printf("\n\t\tEnter Updated Phone number: ");
+            scanf("%lf", &updatedStudent.phonenumber);
+            break;
+
+        case 7:
+            printf("\n\t\tEnter Updated GPA: ");
+            scanf("%f", &updatedStudent.gpa);
+            break;
+
+        default:
+            printf("\n\t\tInvalid choice.\n");
+            goto flag;
+            return;
+    }
+
+    file = fopen(filename, "rb+");
+    if (file == NULL) 
+    {
+        printf("\t\tUnable to open file.\n");
+        return;
+    }
+
+    while (fread(&newstudent, sizeof(struct Student), 1, file))
+    {
+        if (newstudent.id == id)
+        {
+            fseek(file, -sizeof(struct Student), SEEK_CUR);
+            fwrite(&updatedStudent, sizeof(struct Student), 1, file);
+            break;
+        }
+    }
+
+    fclose(file);
+
+    printf("\n\t\tStudent details updated successfully.\n");
+}
+
+
+void DeleteStudent()
+{
+    int search = 0, found = 0;
+    system("cls");
+    printf("\t\t\t\t\t<== Delete Student Record ==>\n\n");
+    
+    printf("\t\tEnter the Id you want to delete: ");
+    scanf("%d", &search);
+    
+    FILE *file = fopen(filename, "rb");
+    if (file == NULL) 
+    {
+        printf("\t\tUnable to open file.\n");
+        return;
+    }
+    
+    FILE *tempFile = fopen("temp.dat","wb");
+    if (tempFile == NULL) 
+    {
+        printf("\t\tUnable to create temporary file.\n");
+        fclose(file);
+        return;
+    }
+    
+    struct Student newstudent;
+    
+    while (fread(&newstudent, sizeof(struct Student), 1, file)) 
+    {
+        if (newstudent.id != search)
+        {
+            fwrite(&newstudent, sizeof(struct Student), 1, tempFile);
+        } 
+        else 
+        {
+            found = 1;
+        }
+    }
+    
+    fclose(file);
+    fclose(tempFile);
+
+    remove(filename);
+    rename("temp.dat", filename);
+
+    if (found) 
+    {
+        printf("\t\tStudent with ID %d deleted successfully.\n", search);
+    } 
+    else 
+    {
+        printf("\t\tStudent with ID %d not found.\n", search);
+    }
+}
